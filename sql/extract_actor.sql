b58f4c998bdd3ad6a8f2e853375aa39e4b8e27c1
RubenNhez <ruben.nhez@gmail.com>
2025-08-13 10:36:38 +0100

    sql queries for extract

---
+++ sql/extract_actor.sql
@@ -0,0 +1,7 @@
+SELECT
+    actor_id,
+    first_name,
+    last_name,
+    last_updated
+FROM 
+    actor;
---
+++ sql/extract_rental.sql
@@ -0,0 +1,10 @@
+SELECT 
+    rental_id, 
+    rental_date, 
+    inventory_id, 
+    customer_id, 
+    return_date, 
+    staff_id, 
+    last_update
+FROM 
+    rental;
